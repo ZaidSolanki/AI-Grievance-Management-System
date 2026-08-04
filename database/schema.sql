8811cd9b9bfee769b5d3a9de6d0831a4f58de783
Zaid <solankizaid63@gmail.com>
2026-08-04 18:25:45 +0530

    Add initial SQLite database schema

---
+++ database/schema.sql
@@ -0,0 +1,142 @@
+
+-- AI Grievance Management System
+-- Database Schema (SQLite)
+
+
+PRAGMA foreign_keys = ON;
+
+
+-- USERS TABLE
+
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    full_name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    phone TEXT UNIQUE,
+    password TEXT NOT NULL,
+    role TEXT NOT NULL DEFAULT 'user'
+        CHECK(role IN ('user', 'admin')),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+-- DEPARTMENTS TABLE
+
+
+CREATE TABLE departments (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    department_name TEXT NOT NULL UNIQUE,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+-- COMPLAINTS TABLE
+
+
+CREATE TABLE complaints (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+    user_id INTEGER NOT NULL,
+
+    title TEXT NOT NULL,
+
+    description TEXT NOT NULL,
+
+    image_path TEXT,
+
+    category TEXT
+        CHECK(category IN (
+            'Road',
+            'Water',
+            'Electricity',
+            'Garbage',
+            'Sewer',
+            'Street Light',
+            'Illegal Parking',
+            'Other'
+        )),
+
+    priority TEXT
+        CHECK(priority IN (
+            'Low',
+            'Medium',
+            'High'
+        )),
+
+    department_id INTEGER,
+
+    status TEXT NOT NULL DEFAULT 'Pending'
+        CHECK(status IN (
+            'Pending',
+            'In Progress',
+            'Resolved'
+        )),
+
+    location TEXT,
+
+    latitude REAL,
+
+    longitude REAL,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY(user_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE,
+
+    FOREIGN KEY(department_id)
+        REFERENCES departments(id)
+        ON DELETE SET NULL
+);
+
+
+-- COMPLAINT HISTORY TABLE
+
+
+CREATE TABLE complaint_history (
+
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+    complaint_id INTEGER NOT NULL,
+
+    updated_by INTEGER NOT NULL,
+
+    old_status TEXT
+    CHECK(old_status IN ('Pending', 'In Progress', 'Resolved')),
+
+    new_status TEXT NOT NULL
+    CHECK(new_status IN ('Pending', 'In Progress', 'Resolved')),
+
+    title TEXT NOT NULL,
+
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY(complaint_id)
+        REFERENCES complaints(id)
+        ON DELETE CASCADE,
+
+    FOREIGN KEY(updated_by)
+        REFERENCES users(id)
+        ON DELETE CASCADE
+);
+
+
+-- INDEXES
+
+
+CREATE INDEX idx_user_email
+ON users(email);
+
+CREATE INDEX idx_complaint_status
+ON complaints(status);
+
+CREATE INDEX idx_department
+ON complaints(department_id);
+
+CREATE INDEX idx_priority
+ON complaints(priority);
